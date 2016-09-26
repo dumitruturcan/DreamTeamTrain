4b928fb3069c5530263654cb505b2d940379fca1
ENDAVA\dturcan <Dumitru.Turcan@endava.com>
2016-09-26 17:47:01 +0300

    Adding Helpers package and Action helpers classes for both contributors

---
+++ src/main/java/Helpers/dturcanHelpers/ActionsHelper.java
@@ -0,0 +1,7 @@
+package Helpers.dturcanHelpers;
+
+/**
+ * Created by dturcan on 9/26/2016.
+ */
+public class ActionsHelper {
+}
---
+++ src/main/java/Helpers/oclunguHelpers/ActionsHelper.java
@@ -0,0 +1,7 @@
+package Helpers.oclunguHelpers;
+
+/**
+ * Created by dturcan on 9/26/2016.
+ */
+public class ActionsHelper {
+}
